861b5c819c04eebbfcffc15b5a9085911720b780
Marcos Alves <alves.henrique.marcos@gmail.com>
2017-04-06 14:13:24 -0300

    Estrutura do projeto refatorada
    + Node.js v7.7.3
    + Express 4.14.1

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -24,7 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/login', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- provas-ufpb-api/business/control/userController.js
+++
@@ -1,5 +0,0 @@
-var user = require('./model/user')
-var infra = require('././infra/exception')
-var addUser = function(login, password){
-	//TODO criterios de senha e password
-}
