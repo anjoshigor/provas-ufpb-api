7f3866e3dcdb77a7b38a35b00f258525294b93dc
Higor Anjos <higor.araujo.anjos@gmail.com>
2017-04-10 21:13:24 -0300

    Resolvendo conflitos de @DiogoDantas

--- routes/login.js
+++ routes/login.js
@@ -1,22 +1,28 @@
 var express = require('express');
 var router = express.Router();
-
 //Importando o modelo do usuario
 var User = require('../business/model/user')
 
 //Importando controller do usuario
-var userController = require('../business/control/userController')
+var UserController = require('../business/control/userController')
 
-var Controller = new userController()
+var controller = new UserController();
 
 router.get('/', (req, res, next) => {
   res.render('login', { title: 'Authentication | API' });
 });
 
 router.post('/', (req, res, next) => {
-  new_user = new User(req.body.login, req.body.password);
-  Controller.add(new_user);
-  res.render('login', { title: 'Authentication | API' });
+  var new_user = new User(req.body.login, req.body.password);
+
+  try {
+    controller.add(new_user);
+    res.render('login', { flash: { type: 'alert-success', msg: 'Login efetuado com sucesso!' } });
+  } catch (error) {
+    res.render('login', { flash: { type: 'alert-danger', msg: error.message } });
+  }
+
 });
+
 
 module.exports = router;
